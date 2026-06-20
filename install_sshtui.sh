02d2d8b88f58eb0dbf0c9fa2db717759167e3516
grzesiowski1223 <grzesiowski1223@gmail.com>
2026-06-21 00:27:45 +0200

    Oh man what a blunder

--- install_sshtui.sh
+++ install_sshtui.sh
@@ -1,39 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
-RED='\033[31m'
-GREEN='\033[0;32m'
-PURPLE='\033[0;35m'
-NC='\033[0m'
+RED=$(echo -e '\033[31m')
+GREEN=$(echo -e '\033[0;32m')
+PURPLE=$(echo -e '\033[0;35m')
+NC=$(echo -e '\033[0m')
 
 echo -e "${PURPLE}sshTUI installer:${NC}"
 read -r -p "Would you like to install the sshTUI? [y/N] " response
 
 case "$response" in
-[yY][eE][sS] | [yY])
-  echo ""
-  echo "-----------------Downloading------------------"
-  echo ""
+    [yY][eE][sS]|[yY])
+        echo ""
+        echo "-----------------Downloading------------------"
+        echo ""
 
-  curl -s -o sshtui https://raw.githubusercontent.com/grzesiowski1223/sshtui/main/sshtui
+        # TUTAJ: Pobieramy aplikację 'sshtui', a nie instalator!
+        curl -s -o sshtui https://raw.githubusercontent.com/grzesiowski1223/sshTUI/main/sshtui
 
-  if [[ ! -f sshtui ]]; then
-    echo -e "${RED}Download failed. Please check the URL or your internet connection.${NC}"
-    exit 1
-  fi
+        if [[ ! -f sshtui ]]; then
+            echo -e "${RED}Download failed. Please check the URL or your internet connection.${NC}"
+            exit 1
+        fi
 
-  chmod +x sshtui
-  
-  if sudo cp sshtui /usr/local/bin/; then
-    echo -e "${GREEN}The installation is successful.${NC}"
-    echo "You can now run 'sshtui' as a command!"
-    rm sshtui
-  else
-    echo -e "${RED}The installation was unsuccessful.${NC}"
-    exit 1
-  fi
-  ;;
-*)
-  echo "Exiting..."
-  exit 0
-  ;;
+        if [[ -n "$ip" ]]; then
+            sed -i "s|^IPADDRESS=.*$|IPADDRESS=\"$ip\"|" sshtui
+        fi
+        
+        chmod +x sshtui
+        sudo cp sshtui /usr/local/bin/
+
+        echo -e "${GREEN}The installation is successful.${NC}"
+        echo "You can now run 'sshtui' as a command!"
+        ;;
+    *)
+        echo "Exiting..."
+        exit
+        ;;
 esac
